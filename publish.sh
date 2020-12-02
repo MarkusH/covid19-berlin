c510eb8d5a5e3a10fe64a68cccaa14f08536d9d4
Markus Holtermann <info@markusholtermann.eu>
2020-12-02 20:12:57 +0100

    Install datasette-vega on publish

--- publish.sh
+++ publish.sh
@@ -4,4 +4,5 @@ datasette publish heroku \
     --title "COVID-19 cases in Berlin, Germany" \
     --name covid19-berlin \
     --metadata metadata.yaml \
+    --install datasette-vega \
     covid19-berlin.db
